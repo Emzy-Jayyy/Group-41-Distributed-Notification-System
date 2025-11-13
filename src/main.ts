7582a8a099af5c8e4efa4994e6d565dedccf1fe0
Aghaulor-Gift <132744631+Aghaulor-Gift@users.noreply.github.com>
2025-11-13 19:24:00 +0100

    fix(tsconfig):remove base url

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   // Global prefix
-  app.setGlobalPrefix('api');
+ // app.setGlobalPrefix('api');
 
   // Validation
   app.useGlobalPipes(
@@ -23,7 +23,7 @@ async function bootstrap() {
   const env = process.env.NODE_ENV || 'development';
   const baseUrl =
     env === 'production'
-      ? process.env.API_BASE_URL || 'https://your-production-domain.com'
+      ? process.env.API_BASE_URL || 'https://distributed-notification-system-user-service-production.up.railway.app'
       : `http://localhost:${port}`;
 
   // Swagger configuration
@@ -44,7 +44,7 @@ async function bootstrap() {
 
   await app.listen(port);
 
-  console.log(`🚀 ${process.env.SERVICE_NAME || 'User Service'} running on port ${port}`);
-  console.log(`📘 Swagger Docs: ${baseUrl}/docs`);
+  console.log(`${process.env.SERVICE_NAME || 'User Service'} running on port ${port}`);
+  console.log(` Swagger Docs: ${baseUrl}/docs`);
 }
 bootstrap();
